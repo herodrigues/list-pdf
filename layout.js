c9db07c16c95712401ea8449438f5c9c465bf737
Herinson Rodrigues <herinson.br@gmail.com>
2019-05-29 15:58:06 -0300

    Rename pdfs (again)

--- layout.js
+++ layout.js
@@ -1,6 +1,11 @@
 const main = document.querySelector(".pdfs");
 
-fetch("https://api.github.com/repos/herodrigues/open-pdf/contents/pdfs")
+// troca pelo teu nome de usuário, repositório e a pasta onde os PDFs estão
+const owner = "herodrigues"
+const repo = "open-pdf"
+const path = "pdfs"
+
+fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`)
   .then(response => response.json()).then(response => {
     response.map(item => {
       var loadingTask = pdfjsLib.getDocument(item.download_url);
@@ -14,6 +19,7 @@ fetch("https://api.github.com/repos/herodrigues/open-pdf/contents/pdfs")
         const span = cln.querySelector("span");
 
         link.onclick = () => window.open(`https://mozilla.github.io/pdf.js/web/viewer.html?file=${item.download_url}`);
+        link.title = `Open this PDF in a new tab`;
         span.textContent = item.name;
         main.querySelector("h2").textContent = "";
         main.appendChild(cln);
